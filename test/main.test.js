761bd5f2389d04560ee84abe38d90ee437858292
Ashley Stanbridge <ashley_stanbridge@yahoo.com>
2019-10-10 17:24:56 -0500

    Made test functions for each function in tipping calculator

---
+++ test/main.test.js
@@ -0,0 +1,15 @@
+QUnit.test( "TEST calcTotal", assert => {
+    assert.equal(calcTotal(20, .05, 2),10.5, "Normal input");
+    assert.equal(calcTotal(-20, .15, -10),2.3, "Negative inputs")
+    assert.equal(calcTotal(30, .2, 2),18, "Normal input")
+    assert.equal(calcTotal(40, .1, 4),11, "Normal input")
+    assert.equal(calcTotal(50, .15, 5),11.5, "Normal input")
+  });
+
+  QUnit.test( "TEST calcTip", assert => {
+    assert.equal(calcTotal(20, .05, 1),1, "Normal input");
+    assert.equal(calcTotal(-20, .15, -10),0.3, "Negative inputs")
+    assert.equal(calcTotal(30, .2, 2),3, "Normal input")
+    assert.equal(calcTotal(40, .1, 4),1, "Normal input")
+    assert.equal(calcTotal(50, .15, 5),1.5, "Normal input")
+  });
